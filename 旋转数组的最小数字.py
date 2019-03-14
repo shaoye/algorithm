99f9510724d1fc35ac2367eeaac3183462323fc1
Shao ye <shaoye1124@gmail.com>
2019-03-14 21:23:24 +0800

    Create 旋转数组的最小数字.py

---
+++ 旋转数组的最小数字.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def minNumberInRotateArray(self, rotateArray):
+        # write code here
+        length = len(rotateArray)
+        if length == 0: return 0
+        if length == 1: return rotateArray[0]
+        left = 0
+        right = length - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if rotateArray[mid] < rotateArray[right]:
+                right = mid
+                continue
+            if rotateArray[mid] > rotateArray[left]:
+                left = mid + 1
+                continue
+            if rotateArray[mid] == rotateArray[right]:
+                right -= 1
+            if rotateArray[mid] == rotateArray[left]:
+                left += 1
+        return rotateArray[right]
