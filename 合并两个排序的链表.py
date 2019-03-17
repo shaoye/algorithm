74b1243a4cdd942c07220ad961dbf88bfd92419e
Shao ye <shaoye1124@gmail.com>
2019-03-17 13:40:00 +0800

    Create 合并两个排序的链表.py

---
+++ 合并两个排序的链表.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回合并后列表
+    def Merge(self, pHead1, pHead2):
+        # write code here
+        cur = dummy = ListNode(0)
+        while pHead1 and pHead2:
+            if pHead1.val <= pHead2.val:
+                cur.next = pHead1
+                pHead1 = pHead1.next
+            else:
+                cur.next = pHead2
+                pHead2 = pHead2.next
+            cur = cur.next
+        cur.next = pHead1 or pHead2
+        return dummy.next
+
