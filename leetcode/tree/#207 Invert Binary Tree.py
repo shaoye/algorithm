c90c55f4cf41f104b23579500344750f1b735c54
Shao ye <shaoye1124@gmail.com>
2019-04-11 15:29:28 +0800

    Create #207 Invert Binary Tree.py

---
+++ leetcode/tree/#207 Invert Binary Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+        tmp = root.right
+        root.right = self.invertTree(root.left)
+        root.left = self.invertTree(tmp)
+        return root
+    
+    def invertTree_iterative(self, root: TreeNode) -> TreeNode:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.extend([node.left,node.right])
+        return root
