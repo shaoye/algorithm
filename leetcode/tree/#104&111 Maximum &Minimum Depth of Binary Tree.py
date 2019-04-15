acdd3f1d20b28dafc1de98d78a6050405c58aae1
Shao ye <shaoye1124@gmail.com>
2019-04-15 10:45:15 +0800

    Update and rename #104 Maximum Depth of Binary Tree.py to #104&111 Maximum &Minimum Depth of Binary Tree.py

--- leetcode/tree/#104&111 Maximum &Minimum Depth of Binary Tree.py
+++ leetcode/tree/#104&111 Maximum &Minimum Depth of Binary Tree.py
@@ -11,3 +11,12 @@ class Solution:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left or not root:
+            return 1+self.minDepth(root.right)
+        if not root.right:
+            return 1+self.minDepth(root.left)
+        return 1+min(self.minDepth(root.left), self.minDepth(root.right))
