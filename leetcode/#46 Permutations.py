6380556d59b362a0871f30dbf65f8da9dfd9be25
Shao ye <shaoye1124@gmail.com>
2019-04-17 21:32:31 +0800

    Create #46 Permutations.py

---
+++ leetcode/#46 Permutations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        ret = []
+        self.dfs(nums, [], ret)
+        return ret
+
+    def dfs(self, nums, path, ret):
+        if not nums:
+            ret.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], ret)
+
+    def permute(self, nums):
+        if len(nums) <= 1:
+            return [nums]
+        ret = []
+        for i, num in enumerate(nums):
+            n = nums[:i] + nums[i+1:]
+            dfs = self.permute(n)
+            for y in dfs:
+                ret.append([num]+y)
+        return ret
